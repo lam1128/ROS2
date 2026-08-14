ac606aac920288b2d40a07562c26e488bbe8f2e7
Julian Gaal <julian.gaal@anavs.de>
2026-08-14 17:37:36 +0200

    Keep only Kria and Jetson workspaces

--- scripts/runtime/run_evt21_decoder.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-cd /home/anavs/ROS2
-. /opt/ros/humble/setup.bash
-. install/setup.bash
-export ROS_DOMAIN_ID=42 RMW_IMPLEMENTATION=rmw_fastrtps_cpp ROS_LOCALHOST_ONLY=0
-exec ros2 run jetson_evt21_decoder evt21_decoder_node
